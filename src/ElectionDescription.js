028e69d7080c17b95b3b2d9ae7048e5cd2c80dfe
aliasunder <32620895+aliasunder@users.noreply.github.com>
2018-07-07 02:32:39 -0400

    syntax updated in ElectionDescription component

--- src/ElectionDescription.js
+++ src/ElectionDescription.js
@@ -19,16 +19,14 @@ const descriptionStyle = {
 const ElectionDescription = ()=>{
     return (
         <Segment className="news__font" style={ cardStyle } basic>
-            <Segment basic>
-                <Header style= { headerStyle }> Ontario voters head to the polls June 7 </Header>
-                <Divider />
-                <Segment basic style={ descriptionStyle }>
-                    <p> Ontarians are heading to the polls on June 7 to vote for the next
-                        provincial government. Television coverage begins at 8:30 p.m. ET
-                        on CBC News Network and CBC Television. We'll run a livestream at
-                        the top of this page.
-                    </p>
-                </Segment>
+            <Header style= { headerStyle }> Ontario voters head to the polls June 7 </Header>
+            <Divider />
+            <Segment basic style={ descriptionStyle }>
+                <p> Ontarians are heading to the polls on June 7 to vote for the next
+                    provincial government. Television coverage begins at 8:30 p.m. ET
+                    on CBC News Network and CBC Television. We'll run a livestream at
+                    the top of this page.
+                </p>
             </Segment>
         </Segment>
     )
